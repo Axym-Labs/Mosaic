f04c540052977ae7821784ac873d2c47507ae026
Davide R. Wiest <davide.wiest2@gmail.com>
2024-03-02 21:56:09 +0100

    move contentPieces to Archived, add new fragments

--- src/db/fragmentDefinitions.php
+++ src/db/fragmentDefinitions.php
@@ -4,36 +4,52 @@ class fragmentDefinitions {
     private $tableConf;
 
     public $credentials;
+    public $projectinfo;
+    public $socials;
     public $images;
-    public $linksection;
-    public $newsSection;
+    public $link;
+    public $news;
+    // public $linksection;
+    // public $newsSection;
     public $texts;
     public $iframes;
 
     public function __construct($dbCon) {
         $this->dbCon = $dbCon;
         $this->tableConf = new tableConfiguration("queries/Fragments/", "Fragment");
-        $this->credentials = new dbTable("credentials", $this->tableConf, $this->dbCon);
-        $this->images = new dbTable("fragmentimage", $this->tableConf, $this->dbCon);
-        $this->linksection = new dbTable("LinkSection", $this->tableConf, $this->dbCon);
-        $this->newsSection = new dbTable("NewsSection", $this->tableConf, $this->dbCon);
+        $this->credentials = new dbTable("Credentials", $this->tableConf, $this->dbCon);
+        $this->projectinfo = new dbTable("ProjectInfo", $this->tableConf, $this->dbCon);
+        $this->socials = new dbTable("Socials", $this->tableConf, $this->dbCon);
+        $this->images = new dbTable("Image", $this->tableConf, $this->dbCon);
+        $this->link = new dbTable("Link", $this->tableConf, $this->dbCon);
+        $this->news = new dbTable("News", $this->tableConf, $this->dbCon);
+        // $this->linksection = new dbTable("LinkSection", $this->tableConf, $this->dbCon);
+        // $this->newsSection = new dbTable("NewsSection", $this->tableConf, $this->dbCon);
         $this->texts = new dbTable("Text", $this->tableConf, $this->dbCon);
         $this->iframes = new dbTable("IFrame", $this->tableConf, $this->dbCon);
     }
 
     public function GetTableByName($tableName) {
-        switch ($tableName) {
+        switch ($tableName->lower()) {
             case "credentials":
                 return $this->credentials;
-            case "fragmentimage":
+            case "ProjectInfo":
+                return $this->projectinfo;
+            case "Socials":
+                return $this->socials;
+            case "Image":
                 return $this->images;
-            case "LinkSection":
-                return $this->linksection;
-            case "NewsSection":
-                return $this->newsSection;
-            case "Text":
+            case "link":
+                return $this->link;
+            case "news":
+                return $this->news;
+            // case "LinkSection":
+            //     return $this->linksection;
+            // case "NewsSection":
+            //     return $this->newsSection;
+            case "text":
                 return $this->texts;
-            case "IFrame":
+            case "iframe":
                 return $this->iframes;
         }
     }
